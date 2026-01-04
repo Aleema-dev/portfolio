d5f07fc35dc41d96776436a8084bbdabb23e18f5
Aleema-dev <azmialeema2007@gmail.com>
2026-01-03 23:04:03 -1200

    upload portfolio files

---
+++ script.js.js
@@ -0,0 +1,12 @@
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("show");
+    });
+});
